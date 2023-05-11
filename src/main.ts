79d0955ed2f5c767ecc445301f1226120bd94943
RimidalU <stankevichuuladimiru@gmail.com>
2023-05-12 01:31:05 +0300

    feat: add users to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 5000;
@@ -7,6 +9,15 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule, { cors: false });
     app.enableCors({ credentials: true, origin: true });
+
+    const config = new DocumentBuilder()
+      .setTitle('User badge creator')
+      .setDescription('Badge generator from user database.')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);

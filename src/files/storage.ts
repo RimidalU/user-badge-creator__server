5ae35bf80e1e4d0a4a4277921df5c3844f567ff6
RimidalU <stankevichuuladimiru@gmail.com>
2023-05-14 14:05:01 +0300

    fix: fix file name generator

--- src/files/storage.ts
+++ src/files/storage.ts
@@ -3,9 +3,7 @@ import { diskStorage } from 'multer';
 const generateId = () =>
   Array(18)
     .fill(null)
-    .map(() => {
-      Math.round(Math.random() * 16).toString(16);
-    })
+    .map(() => Math.round(Math.random() * 16).toString(16))
     .join('');
 
 const normalizeFileName = (req, file: Express.Multer.File, callback) => {

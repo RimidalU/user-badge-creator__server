88dc503564470c51dee04cc3eea1fad2a87bac5c
RimidalU <stankevichuuladimiru@gmail.com>
2023-05-15 23:43:08 +0300

    feat: implement getme rout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   Delete,
   UseGuards,
+  Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserId } from 'src/auth/decorators/userId.decorator';
 
 @Controller('users')
 @ApiTags('users')
@@ -18,22 +20,29 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post('/me')
+  @UseGuards(JwtAuthGuard)
+  async getMe(@UserId() id: any) {
+    console.log(id);
+    return await this.usersService.findById(+id);
+  }
+
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.usersService.findById(+id);
+  async findOne(@Param('id') id: number) {
+    return await this.usersService.findById(+id);
   }
 
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBody({ type: UpdateUserDto })
-  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return await this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
-  remove(@Param('id') id: number) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: number) {
+    return await this.usersService.remove(+id);
   }
 }
